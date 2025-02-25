1779b976d46c4206dc47298160a8e32e02a910d5
shenal-anthony <fonseka-im21039@stu.kln.ac.lk>
2025-02-25 18:33:28 +0530

    change the App.jsx

--- client/my-app/src/App.jsx
+++ client/my-app/src/App.jsx
@@ -1,33 +1,28 @@
 import React from "react";
-import "./App.css";
 import AdminLayout from "./layouts/AdminLayout";
 import AdminDashboard from "./pages/AdminDashboard";
 // import GuideLayout from "./layouts/GuideLayout";
 // import GuideDashboard from "./pages/GuideDashboard";
 
 // Dummy role (replace with real auth logic)
-// const userRole = "admin"; // Change to "admin" for testing
+const userRole = "admin"; // Change to "admin" for testing
 
 function App() {
-  return (
-    // userRole === "admin" ?
-    // (
+  return userRole === "admin" ? (
     <>
-      <h2>Admin</h2>
       <AdminLayout>
+      <h2>Admin</h2>
         <AdminDashboard />
       </AdminLayout>
     </>
+  ) : (
+    <>
+      <h1>Guide</h1>
+      <GuideLayout>
+        <GuideDashboard />
+      </GuideLayout>
+    </>
   );
-  // )
-  // : (
-  //   <>
-  //     <h1>Guide</h1>
-  //     <GuideLayout>
-  //       <GuideDashboard />
-  //     </GuideLayout>
-  //   </>
-  // )
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,16 +1,19 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
-
-require('dotenv').config();
-
 const app = express();
+const PORT = process.env.PORT || 8000;
 
-app.use(cors());
+
+// Middlewares
+app.use(cors({origin: 'http://localhost:5173'}));
 app.use(bodyParser.json());
 
+// Routes
 app.use('/api/users', userRoutes);
 
-const PORT = process.env.PORT || 8000;
+// Error handler
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
