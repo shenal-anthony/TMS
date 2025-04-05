12f8c78ccac6d6532276b93be35834927f2c13c7
shenal-anthony <fonseka-im21039@stu.kln.ac.lk>
2025-04-05 16:32:03 +0530

    add JWT_SECRET_02 to environment variables and implement token generation utility

--- server/utils/tokenGenerator.js
+++ server/utils/tokenGenerator.js
@@ -4,9 +4,11 @@ require("dotenv").config();
 const generateToken = (userId) => {
   const payload = { userId };
 
-  const newToken = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1h" });
+  const newToken = jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
   // console.log("Token:", newToken); // Debug
   return newToken;
 };
 
-module.exports = generateToken;
+module.exports = { generateToken };
